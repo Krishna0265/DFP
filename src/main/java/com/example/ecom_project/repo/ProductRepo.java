283b896dd0328ee17286c5b5cd7c85f844f51536
Mylavarapu <2494402@cognizant.com>
2026-05-14 11:29:04 +0530

    Added Product Repo

---
+++ src/main/java/com/example/ecom_project/repo/ProductRepo.java
@@ -0,0 +1,12 @@
+package com.example.ecom_project.repo;
+
+import com.example.ecom_project.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductRepo extends JpaRepository<Product,Integer> {
+
+}
